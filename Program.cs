d9cc8c0280e11e40830bdf0863ced81007d837bc
DanielCuyuch07 <danielproyectos13@gmail.com>
2024-02-14 22:43:32 -0600

    [UPDATE] Version V1 del proyecto

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecturaDeArchivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leer texto completo
+            string texto = System.IO.File.ReadAllText(@"C:\Users\danie\Desktop\TextoParaC.txt");
+            Console.WriteLine("El archivo contiene el siguiente texto: {0}", texto);
+
+            // Leer de a lienas
+            string[] lineas = System.IO.File.ReadAllLines(@"C:\Users\danie\Desktop\TextoParaC.txt");
+            foreach (string line in lineas) { 
+                Console.WriteLine("\t" + line);
+            
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
